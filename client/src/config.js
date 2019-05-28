30ca2db09282e196baf214b596b9565844b3757b
Erikdzo <erikdzo@hotmail.com>
2019-05-28 11:59:43 +0300

    Change port

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/postgres-query/api' : 'http://apex.ttu.ee:8080/postgres-query/api';
+export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/postgres-query/api' : 'http://apex.ttu.ee:8080/postgres-query/api';
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = 8080;
 const mountRoutes = require('./routes');
 const helmet = require('helmet');
 
